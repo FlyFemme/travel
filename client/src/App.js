e8b6c4c6e342229b67f1820e66dba6ea79c1e38b
leandrita <lea.bujh@gmail.com>
2023-08-29 22:03:48 +0200

    add home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './pages/home/home.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/create' element={<CreateCard />} /> */}
+          {/* <Route path='/edit/:id' element={<EditCard />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/home/home.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/navbar/navbar';
+import './home.css';
+
+const endpoint = 'http://localhost:8000/api'
+const Home = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        getAllCards()
+    }, [])
+
+    const getAllCards = async () => {
+        const response = await axios.get(`${endpoint}/cards`)
+        setCards(response.data)
+    }
+
+    // const deleteCard = async (id) => {
+    //     await axios.delete(`${endpoint}/card/${id}`)
+    //     getAllCards()
+    // }
+
+    return (
+        <>
+            <Navbar />
+            <main className="d-flex flex-wrap justify-content-center" >
+                {cards.map((card) => (
+                    <div className=" item b-radius m-3" key={card.id}>
+                        <img className="b-radius size" src={card.image} alt="" />
+                        <div className="card-body">
+                            <h5 className="card-title">{card.title}</h5>
+                            <p className="card-text">{card.location}</p>
+                        </div>
+                    </div>
+                ))}
+            </main>
+        </>
+    )
+}
+
+export default Home
