803822d67a6dd0424012e6886a9d7a8b40bfc8af
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-30 21:07:13 +0200

    pull dev/fix naming

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import ShowLogged from './pages/ShowLogged/ShowLogged';
-import Home from './pages/home/Home'
+import ShowLogged from './pages/ShowLogged/ShowLogged';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
 
           {/* <Route path='/edit/:id' element={<EditCard />} /> */}
 
-
+          <Route path='/show-logged' element={<ShowLogged />} />
           </Routes>
         </BrowserRouter>
       </div>
