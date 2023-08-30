414bfa682c3317904020da4354f9320467f22f10
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-30 08:33:06 +0200

    routes fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/show-logged" />
+        <Routes path="/show-logged" />
 
-        <Route path='/' element={<Home />} />
+        <Routes path='/' element={<Home />} />
           {/* <Route path='/create' element={<CreateCard />} /> */}
           {/* <Route path='/edit/:id' element={<EditCard />} /> */}
           
