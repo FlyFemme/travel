52d566b9af6745de592b59c3601f2071c72757b1
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-30 14:25:12 +0200

    refactoring showLogged

--- client/src/pages/ShowLogged/ShowLogged.jsx
+++ client/src/pages/ShowLogged/ShowLogged.jsx
@@ -1,25 +1,88 @@
 import React from 'react';
 import './ShowLogged.css';
-import Navbar from '../../components/Navbar/Navbar';
 import Card from '../../components/Card/Card';
+import Navbar from '../../components/Navbar/Navbar';
 
 const ShowLogged = () => {
   const show = {
     image: 'url_de_la_imagen.jpg',
     title: 'Título del Show',
-    text: 'Descripción del show...', 
+    location: 'Ubicación del Show',
+    content: 'Descripción del show...',
   };
+
+  const isUserAuthenticated = true; //Aqui queda true por que es una vista Logged //
 
   return (
     <>
       <Navbar />
       <div className="show-page">
-        <Card
-          imageSrc={show.image}
-          title={show.title}
-          text={show.text}
-        />
+        <div className="container" style={{ marginTop: '4rem' }}>
+          <div className="row">
+            <div className="col-md-6">
+              <div
+                className="d-flex justify-content-center align-items-center"
+                style={{ marginRight: '-10rem' }}
+              >
+                <img
+                  className="rounded-4"
+                  src={show.image}
+                  alt={show.title}
+                  width="400"
+                  height="400"
+                />
+              </div>
+            </div>
+            <div className="col-md-5 d-flex flex-column">
+              <div className="d-flex justify-content-between align-items-center">
+                <h3 style={{ color: '#FF0060', fontWeight: 'bold' }}>
+                  {show.title}
+                </h3>
+                <div className="d-flex">
+                  {isUserAuthenticated && (
+                    <>
+                      <a href={`edit/${show.id}`}>
+                        <img
+                          src={require('../../Assets/Edit-icon.svg').default}
+                          className="m-1"
+                          alt=""
+                        />
+                      </a>
+                      <form
+                        action={`delete/${show.id}`}
+                        method="POST"
+                        style={{ display: 'inline' }}
+                      >
+                        {/* Agregar el token CSRF y método DELETE según corresponda */}
+                        <button
+                          type="submit"
+                          className="delete-destination"
+                          style={{
+                            border: 'none',
+                            backgroundColor: 'transparent',
+                          }}
+                        >
+                          <img
+                            src={require('../../Assets/Delete-icon.svg').default}
+                            alt="delete-icon"
+                          />
+                        </button>
+                      </form>
+                    </>
+                  )}
+                </div>
+              </div>
+              <p style={{ color: '#FF0060' }}>{show.location}</p>
+              <p className="text-primary">{show.content}</p>
+            </div>
+          </div>
+        </div>
       </div>
+      <Card
+        imageSrc={show.image}
+        title={show.title}
+        text={show.content}
+      />
     </>
   );
 };
