6e0d02db276579a97eb8654868edbb2c81108207
leandrita <lea.bujh@gmail.com>
2023-09-01 01:19:33 +0200

    test

---
+++ client/src/pages/Create/Create.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/navbar/Navbar';
+import CreateForm from '../../components/CreateForm/CreateForm';
+import './Edit.css';
+
+const Create = () => {
+
+    return (
+        <>
+            <Navbar />
+            <CreateForm />
+        </>
+    );
+};
+
+export default Create;
