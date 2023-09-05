76e56b3721b99d6a66def1cc595288b40a633309
LucíaSilva <coderalsc@gmail.com>
2023-09-05 09:29:49 +0200

    instalation of package

--- server/routes/web.php
+++ server/routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CardController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SearchGuestController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\BlogController;
 
 
 Route::get('/', [CardController::class, 'index'])->name('cards');
@@ -13,20 +15,20 @@ Route::get('/', [CardController::class, 'index'])->name('cards');
 Route::get('/dashboard', [FormAddNewCardController::class, 'dashboard'])
     ->middleware(['auth'])
     ->name('dashboard');
-    
+
 Route::get('/details/{formAddNewCard}', [FormAddNewCardController::class, 'details'])
     ->middleware(['auth'])
     ->name('details');
-    
+
 Route::resource('formAddNewCard', FormAddNewCardController::class)
     ->only(['index', 'store', 'edit', 'update', 'destroy'])
     ->middleware(['auth', 'verified']);
-    
+
 Route::get('/search', [SearchController::class, 'search'])->name('search');
 Route::get('/searchGuest', [SearchGuestController::class, 'searchGuest'])->name('searchGuest');
 
 
-    
+
 require __DIR__.'/auth.php';
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
@@ -35,5 +37,19 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//grupo de rutas protegidas para los controladores
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RolController::class);
+    Route::resource('usuarios', UsuarioController::class);
+    Route::resource('blogs', BlogController::class);
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
